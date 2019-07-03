13514d6dca29a1e3e1f5b7f5f9f9fb74416718d0
CapGalius <42454660+CapGalius@users.noreply.github.com>
2019-07-03 11:36:35 -0300

    Add files via upload

--- vendas.py
+++ vendas.py
@@ -1,26 +1,46 @@
 # programa de exercícios
 
-print("----------------------------------------")
-print("------------------Programa--------------")
-print("---------------------de-----------------")
-print("-------------------Vendas---------------")
-print("----------------------------------------")
+print("-------------------------------------------------")
+print("-----------------------Programa------------------")
+print("--------------------------de---------------------")
+print("------------------------Vendas-------------------")
+print("-------------------------------------------------")
 
 fatura = []
-venda = 0
 total = 0
- 
-print(" Aperte 1 para venda.")
-print(" Aperte 2 para final.")
-venda = int(input("Digite a opção: "))
-while venda == 1:
-    produto = input("Digite o nome do produto: ")
-    preco = float(input("Digite o preço do produto: "))
-    fatura.append([produto,preco])       
-    total += preco
-    venda = int(input("Deseja comprar mais um produto? (1-SIM) (2-NÃO)"))
+venda = ""
+iniciochk = False
 
+print("Aperte 1 para venda.")
+print("Aperte 2 para final.")
+
+while iniciochk == False:
+    try:
+        venda = int(input("Digite a opção: "))
+    except ValueError:
+        print("Digite apenas 1 para venda ou 2 para encerrar.")
+    try:
+        if venda == 1 or venda == 2:
+            iniciochk = True
+            break
+     
+    except ValueError:
+        print("Digite apenas 1 para venda ou 2 para encerrar.")
+        iniciochk = False  
+        
+while venda is not 2:
+    try:    
+        produto = input("Digite o nome do produto: ")
+        preco = float(input("Digite o preço do produto: "))
+        fatura.append([produto,preco])       
+        total += preco
+        venda = int(input("Deseja comprar mais um produto? (1-SIM) (2-NÃO)"))
+    except ValueError:
+        print("Repita a operação. No campo preço")
+        print("insira somente números.  Centavos use '.' ")
+        
 print("Seus produtos comprados foram: ")
+print("-"*50)
 for i in fatura:
     print(i[0],"-","R$",i[1])
 print(f"O total de sua fatura foi R$",total)
