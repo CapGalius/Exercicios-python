3c195e8e2ce63ec683d8876157cfab8111b2d34c
CapGalius <capgalius@gmail.com>
2019-07-19 16:57:06 -0300

    corerção de erros digitação sexo

--- imc.py
+++ imc.py
@@ -10,17 +10,26 @@ while True:
     try:
         peso = float(input("Insira seu peso: "))
         altura = float(input("Insira sua altura: "))
-        sexo = int(input(" Digite 1 para Mas. ou 2 para Fem.: "))
         break
     except Exception as e:
         print("Dados errados. Repita a operação!")
     
 def CIMC():
-    if sexo == 1:
-        lista = [20.7, 26.4, 27.8, 31.1]
-    elif sexo == 2:
-        lista = [19.1, 25.8, 27.3, 32.3]
-    
+    valid_sexo = False
+    while valid_sexo == False:
+        try:
+            sexo = int(input("Digite 1 para Mas. ou 2 para Fem.: "))
+            if sexo == 1 or sexo == 2:
+                valid_sexo = True
+                if sexo == 1:
+                    lista = [20.7, 26.4, 27.8, 31.1]
+                elif sexo == 2:
+                    lista = [19.1, 25.8, 27.3, 32.3]
+                else:
+                    print("Erro de operação. Repita.")            
+        except Exception as e:
+              print("Erro de operação. Repita.")
+ 
     imc = peso / (altura**2)
     print("-"*25)
     print("O valor de seu IMC é {:.2f}".format(imc))
