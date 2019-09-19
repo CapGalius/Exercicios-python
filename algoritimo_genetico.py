2d8b77b606e6c27930dba79678d022c7069e9d03
CapGalius <capgalius@gmail.com>
2019-09-19 08:42:54 -0300

    00

--- algoritimo_genetico.py
+++ algoritimo_genetico.py
@@ -154,8 +154,8 @@ class AlgoritmoGenetico():
         
 if __name__ == '__main__':
     lista_produtos = []
-    '''
-    conexao = pymysql.connect(host ='localhost', user = 'root', passwd = 'P4$$w0rd', db = 'produtos')
+    
+    conexao = pymysql.connect(host ='localhost', user = 'usuario', passwd = 'password', db = 'produtos')
     cursor = conexao.cursor()
     cursor.execute('select nome, espaco, valor, quantidade from produtos')
     
@@ -164,23 +164,22 @@ if __name__ == '__main__':
     
     cursor.close()
     conexao.close()
-    '''
     
     
-    lista_produtos.append(Produto("Geladeira Dako", 0.751, 999.90))
-    lista_produtos.append(Produto("Iphone 6", 0.0000899, 2911.12))
-    lista_produtos.append(Produto("TV 55' ", 0.400, 4346.99))
-    lista_produtos.append(Produto("TV 50' ", 0.290, 3999.90))
-    lista_produtos.append(Produto("TV 42' ", 0.200, 2999.00))
-    lista_produtos.append(Produto("Notebook Dell", 0.00350, 2499.90))
-    lista_produtos.append(Produto("Ventilador Panasonic", 0.496, 199.90))
-    lista_produtos.append(Produto("Microondas Electrolux", 0.0424, 308.66))
-    lista_produtos.append(Produto("Microondas LG", 0.0544, 429.90))
-    lista_produtos.append(Produto("Microondas Panasonic", 0.0319, 299.29))
-    lista_produtos.append(Produto("Geladeira Brastemp", 0.635, 849.00))
-    lista_produtos.append(Produto("Geladeira Consul", 0.870, 1199.89))
-    lista_produtos.append(Produto("Notebook Lenovo", 0.498, 1999.90))
-    lista_produtos.append(Produto("Notebook Asus", 0.527, 3999.00)) 
+    #lista_produtos.append(Produto("Geladeira Dako", 0.751, 999.90))
+    #lista_produtos.append(Produto("Iphone 6", 0.0000899, 2911.12))
+    #lista_produtos.append(Produto("TV 55' ", 0.400, 4346.99))
+    #lista_produtos.append(Produto("TV 50' ", 0.290, 3999.90))
+    #lista_produtos.append(Produto("TV 42' ", 0.200, 2999.00))
+    #lista_produtos.append(Produto("Notebook Dell", 0.00350, 2499.90))
+    #lista_produtos.append(Produto("Ventilador Panasonic", 0.496, 199.90))
+    #lista_produtos.append(Produto("Microondas Electrolux", 0.0424, 308.66))
+    #lista_produtos.append(Produto("Microondas LG", 0.0544, 429.90))
+    #lista_produtos.append(Produto("Microondas Panasonic", 0.0319, 299.29))
+    #lista_produtos.append(Produto("Geladeira Brastemp", 0.635, 849.00))
+    #lista_produtos.append(Produto("Geladeira Consul", 0.870, 1199.89))
+    #lista_produtos.append(Produto("Notebook Lenovo", 0.498, 1999.90))
+    #lista_produtos.append(Produto("Notebook Asus", 0.527, 3999.00)) 
     
     
     espacos = []
