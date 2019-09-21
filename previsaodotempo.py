4deaafe57fe06252c1212747ca4e6a94fcc3a3ec
CapGalius <capgalius@gmail.com>
2019-09-21 11:16:05 -0300

    subindo

--- previsaodotempo.py
+++ previsaodotempo.py
@@ -8,12 +8,16 @@ def pegarCoordenadas():
         r = requests.get('http://www.geoplugin.net/json.gp')
         if (r.status_code != 200):
             print("Não foi possivel obter localização!")
+            return None
         else:
-            localizacao = json.loads(r.text)
-            coordenadas = {}
-            coordenadas['lat'] = localizacao['geoplugin_latitude']
-            coordenadas['long'] = localizacao['geoplugin_longitude']
-            return coordenadas
+            try:
+                localizacao = json.loads(r.text)
+                coordenadas = {}
+                coordenadas['lat'] = localizacao['geoplugin_latitude']
+                coordenadas['long'] = localizacao['geoplugin_longitude']
+                return coordenadas
+            except:
+                return None
 
 def pegarCodigoLocal(lat,long):
 
@@ -24,15 +28,18 @@ def pegarCodigoLocal(lat,long):
     r = requests.get(LocationAPIUrl)
     if (r.status_code != 200):
         print("Não foi possivel obter Código do local !")
+        return None
     else:
-        locationResponse = json.loads(r.text)
-        infoLocal = {}
-        infoLocal['nomeLocal'] = locationResponse['LocalizedName'] + ", " \
+        try:
+            locationResponse = json.loads(r.text)
+            infoLocal = {}
+            infoLocal['nomeLocal'] = locationResponse['LocalizedName'] + ", " \
             + locationResponse['AdministrativeArea']['LocalizedName'] + ". " \
             + locationResponse['Country']['LocalizedName'] + "."
-        infoLocal['codigoLocal'] = locationResponse['Key']
-        return infoLocal
-
+            infoLocal['codigoLocal'] = locationResponse['Key']
+            return infoLocal
+        except:
+            return None
 
 def pegarTempoAgora(codigoLocal, nomeLocal):
 
@@ -42,19 +49,26 @@ def pegarTempoAgora(codigoLocal, nomeLocal):
 
     r = requests.get(CurrentConditionsAPIUrl)
     if (r.status_code != 200):
-        print("Não foi possivel obter Código do local !")
+        print("Não foi possivel obter Clima Atual !")
+        return None
     else:
-        CurrentConditionsResponse = json.loads(r.text)
-        infoClima = {}
-        infoClima['textoClima'] = CurrentConditionsResponse[0]['WeatherText']
-        infoClima['temperatura'] = CurrentConditionsResponse[0]['Temperature']['Metric']['Value']
-        infoClima['nomeLocal'] = nomeLocal
-        return infoClima
+        try:
+            CurrentConditionsResponse = json.loads(r.text)
+            infoClima = {}
+            infoClima['textoClima'] = CurrentConditionsResponse[0]['WeatherText']
+            infoClima['temperatura'] = CurrentConditionsResponse[0]['Temperature']['Metric']['Value']
+            infoClima['nomeLocal'] = nomeLocal
+            return infoClima
+        except:
+            return None
 
-coordenadas = pegarCoordenadas()
-local = pegarCodigoLocal(coordenadas['lat'], coordenadas['long'])
-climaAtual = pegarTempoAgora(local['codigoLocal'],local['nomeLocal'])
+try:
+    coordenadas = pegarCoordenadas()
+    local = pegarCodigoLocal(coordenadas['lat'], coordenadas['long'])
+    climaAtual = pegarTempoAgora(local['codigoLocal'],local['nomeLocal'])
+    print('Clima atual em: ' + climaAtual['nomeLocal'])
+    print(climaAtual['textoClima'])
+    print('temperatura: ' + str(climaAtual['temperatura']) + "\xb0" + "C")
+except:
+    print("Erro ao processar a solicitação entre em contato com o Suporte!")
 
-print('Clima atual em: ' + climaAtual['nomeLocal'])
-print(climaAtual['textoClima'])
-print('temperatura: ' + str(climaAtual['temperatura']) + "\xb0" + "C")
