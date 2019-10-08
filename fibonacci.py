97255cb23dcc7d0058f1d56c69e3cf21f1c553cc
galileu ramos freitas <galileu@localhost.localdomain>
2019-10-08 17:06:17 -0300

    fib ratio

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,4 @@
-fibonacci_cache = {}
+'''fibonacci_cache = {}
 def fibonacci(n):
 # If we cache the valuem then return it
     if n in fibonacci_cache:
@@ -13,5 +13,26 @@ def fibonacci(n):
 # Cache the value and return it
     fibonacci_cache[n] = value
     return value
-for n in range(1, 101):
+for n in range(1, 1001):
+    print(n, ":", fibonacci(n))'''
+
+from functools import lru_cache
+@lru_cache(maxsize = 10000)
+def fibonacci(n):
+    # Check if imput is positive int
+    if type(n) != int:
+         raise TypeError("n must be a positive int")
+    if n < 1:
+        raise TypeError("n must be a positive int")
+
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    elif n > 2:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+for n in range(1, 51):
     print(n, ":", fibonacci(n))
+    
+print(fibonacci(n+1) / fibonacci(n))
