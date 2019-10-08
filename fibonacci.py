db510c85e4db467d57e322fa6a2103d8bfe19711
galileu ramos freitas <galileu@localhost.localdomain>
2019-10-08 16:42:20 -0300

    SUbindo

---
+++ fibonacci.py
@@ -0,0 +1,18 @@
+fibonacci_cache = {}
+
+def fibonacci(n):
+# If we cache the valuem then return it
+    if n in fibonacci_cache:
+        return fibonacci_cache[n]
+# COmpute the Nth term        
+if n == 1:
+    return 1
+elif n == 2:
+    return 1
+elif n > 2:
+    value =  fibonacci(n-1) + fibonacci(n -2)
+# Cache the value and return it
+fibonacci_cache[n] = value
+
+for n in range(1, 101):
+     print(n, ":", fibonacci(n))
