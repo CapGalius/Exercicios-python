f7cadff8d097a6749ef27fbeefc628a04cfa7f31
galileu ramos freitas <galileu@localhost.localdomain>
2019-10-09 09:02:49 -0300

    --- Auto-Git Commit ---

--- csvprog.py
+++ csvprog.py
@@ -1,11 +1,22 @@
 import csv
+from datetime import datetime
 
 path = "/home/galileu/Exercicios-python/Google Stock Market Data - google_stock_data.csv.csv"
 file = open(path, newline='')
 reader = csv.reader(file)
 header = next(reader) # Fist line is the reader
-data = [row for row in reader] # Read reamining data
-print(header)
+data = []
+for row in reader:
+    # row = [Date, Open, High, Low , Close, Volume, Adj. Close]
+    date =  datetime.strptime(row[0], '%m/%d/%Y')
+    open_price =  float(row[1]) #open is a builtin func 
+    high = float(row[2])
+    low = float(row[3])
+    close = float(row[4])
+    volume = int(row[5])
+    adj_close = float(row[6])
+
+    data.append([date, open_price,high,low,close,volume,adj_close])
 print(data[0])
 
 
