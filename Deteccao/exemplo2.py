34e01142596f6383a7103cdaa417eb847f3ca9c8
Galileu Ramos Freitas <galileu@localhost.localdomain>
2019-10-16 14:25:28 -0300

    1

--- Deteccao/exemplo2.py
+++ Deteccao/exemplo2.py
@@ -3,7 +3,7 @@ import cv2
 classificadorFace = cv2.CascadeClassifier('/home/galileu/Exercicios-python/Deteccao/cascades/haarcascade_frontalface_default.xml')
 classificadorOlhos = cv2.CascadeClassifier('/home/galileu/Exercicios-python/Deteccao/cascades/haarcascade_eye.xml')
 
-imagem = cv2.imread('/home/galileu/Exercicios-python/Deteccao/pessoas/beatles.jpg')
+imagem = cv2.imread('/home/galileu/Exercicios-python/Deteccao/pessoas/pessoas2.jpg')
 imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
 facesDetectadas = classificadorFace.detectMultiScale(imagemCinza)
@@ -12,7 +12,7 @@ for (x, y, l, a) in facesDetectadas:
     imagem = cv2.rectangle(imagem, (x, y), (x + l, y + a), (255, 0, 0), 2)
     regiao = imagem[y:y + a, x:x + l]
     regiaoCinzaOlho = cv2.cvtColor(regiao, cv2.COLOR_BGR2GRAY)
-    olhosDetectados = classificadorOlhos.detectMultiScale(regiaoCinzaOlho, scaleFactor=1.08, minNeighbors=3)
+    olhosDetectados = classificadorOlhos.detectMultiScale(regiaoCinzaOlho, scaleFactor=1.01, minNeighbors=2)
     print(olhosDetectados)
     for (ox,oy, ol, oa) in  olhosDetectados:
         cv2.rectangle(regiao,(ox,oy), (ox + ol, oy + oa), (255,0,255), 2)
