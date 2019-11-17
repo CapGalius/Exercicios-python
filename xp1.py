8bd92158b09b25ecc1677312fa896c5e93e7075b
capgalius <capgalius@gmail.com>
2019-11-17 00:15:23 -0300

    loop

--- xp1.py
+++ xp1.py
@@ -10,4 +10,4 @@ root = Tk()             #cria a aplicação raiz. É uma janela com barra de tí
 w = Label(root, text="Tudo bem até aqui!")    #cria um label com o texto especificado
 w.pack()                                      #insere na tela
 
-root.mainloop()                               #inicializa o loop
+root.mainloop()                               #inicializa o loop loop 
