720d4a352424708d2877f4a43b3cb6e731431ad2
capgalius <capgalius@gmail.com>
2019-11-20 22:44:17 -0300

    12

--- Deteccao/treinamento.py
+++ Deteccao/treinamento.py
@@ -1,10 +1,4 @@
 import cv2
-
-# this is just to unconfuse pycharm
-try:
-     import cv2.__init__ as cv2
-except ImportError:
-     pass
 import os
 import numpy as np 
 
@@ -13,7 +7,12 @@ fisherface = cv2.face.FisherFaceRecognizer_create()
 lbph = cv2.face.LBPHFaceRecognizer_create()
 
 def getImagemComId():
-    caminhos =[os.path.join('fotos',f) for f in os.listdir('Deteccao/fotos')]
+    caminhos =[os.path.join('fotos',f) for f in os.listdir('/home/galileu/Exercicios-python/Deteccao/fotos')]
     print(caminhos)
-
+    faces= []
+    ids = []
+    for caminhoImagem in caminhos:
+        imagemFace = cv2.imread(caminhoImagem)
+        cv2.imshow("Face", imagemFace)
+        cv2.waitKey(10)
 getImagemComId()
