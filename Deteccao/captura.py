43a50ba3f6aedf23639d4192faf7004fc718ec53
capgalius <capgalius@gmail.com>
2019-11-25 15:32:44 -0300

    up

--- Deteccao/captura.py
+++ Deteccao/captura.py
@@ -1,8 +1,8 @@
 import cv2
-import numpy as np
+#import numpy as np
 
 classificador = cv2.CascadeClassifier("/home/galileu/Exercicios-python/Deteccao/cascades/haarcascade_frontalface_default.xml")
-classificaorOlho = cv2.CascadeClassifier("/home/galileu/Exercicios-python/Deteccao/cascades/haarcascade_eye.xml")
+#classificaorOlho = cv2.CascadeClassifier("/home/galileu/Exercicios-python/Deteccao/cascades/haarcascade_eye.xml")
 camera = cv2.VideoCapture(1)
 amostra = 1
 numeroAmostras = 25
@@ -17,17 +17,17 @@ while (True):
     for (x , y , l, a) in facesDetectadas:
         cv2.rectangle(imagem, (x, y), (x + l, y + a), (0, 0, 255), 3)
         regiao = imagem [y:y+a,x:x+l]
-        regiaoCinzaOlho = cv2.cvtColor(regiao, cv2.COLOR_BGR2GRAY)
-        olhosDetectados = classificaorOlho.detectMultiScale(regiaoCinzaOlho)
-        for (ox, oy,ol, oa) in olhosDetectados:
-            cv2.rectangle(regiao, (ox, oy), (ox + ol, oy + oa), (0, 255, 0), 2)
+        #regiaoCinzaOlho = cv2.cvtColor(regiao, cv2.COLOR_BGR2GRAY)
+        #olhosDetectados = classificaorOlho.detectMultiScale(regiaoCinzaOlho)
+        #for (ox, oy,ol, oa) in olhosDetectados:
+        #    cv2.rectangle(regiao, (ox, oy), (ox + ol, oy + oa), (0, 255, 0), 2)
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                if np.average(imagemCinza) > 70:
-                    imagemFace = cv2.resize(imagemCinza[y:y + a, x:x + l], (largura, altura))
-                    cv2.imwrite("Deteccao/fotos/pessoa." +str(id) + "." + str(amostra) + ".jpg",imagemFace)
-                    print("[foto" + str(amostra) + " capturada com sucesso]")
-                    amostra +=1
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+        #if np.average(imagemCinza) > 70:
+            imagemFace = cv2.resize(imagemCinza[y:y + a, x:x + l], (largura, altura))
+            cv2.imwrite("Deteccao/fotos/pessoa." +str(id) + "." + str(amostra) + ".jpg",imagemFace)
+            print("[foto" + str(amostra) + " capturada com sucesso]")
+            amostra +=1
 
 
     cv2.imshow("Face", imagem)
--- Deteccao/treinamento.py
+++ Deteccao/treinamento.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2 
 import os
 import numpy as np 
 
@@ -8,11 +8,31 @@ lbph = cv2.face.LBPHFaceRecognizer_create()
 
 def getImagemComId():
     caminhos =[os.path.join('fotos',f) for f in os.listdir('/home/galileu/Exercicios-python/Deteccao/fotos')]
-    print(caminhos)
+    #print(caminhos)
     faces= []
     ids = []
     for caminhoImagem in caminhos:
-        imagemFace = cv2.imread(caminhoImagem)
-        cv2.imshow("Face", imagemFace)
-        cv2.waitKey(10)
-getImagemComId()
+        imagemFace= cv2.cvtColor(cv2.imread(caminhoImagem) ,cv2.COLOR_RGB2BGR)
+        id = int(os.path.split(caminhoImagem[-1].split['.'])[1])
+        #print(id)
+        ids.append(id)
+        faces.append(imagemFace)
+        #cv2.imshow("Face", imagemFace)
+        #cv2.waitKey(10)
+        return np.array(ids), faces
+ids, faces = getImagemComId()
+#print(faces)
+print("Treinando...")
+eigenface.train(faces, ids)
+eigenface.write('classificadorEigen.yml')
+
+fisherface.train(faces, ids)
+fisherface.write('classificadorfisher.yml')
+
+lbph.train(faces, ids)
+lbph.write('classificadorLBPH.yml')
+
+print('Treinamento realizado.')
+
+
+
