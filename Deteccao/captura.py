25b329a684047ad8c27536d4047c7070e16bf0da
Galileu Ramos Freitas <galileu@localhost.localdomain>
2019-12-01 12:33:51 -0300

    11

--- Deteccao/captura.py
+++ Deteccao/captura.py
@@ -12,7 +12,7 @@ print('capturando as faces...')
 while (True):
     conectado, imagem = camera.read()
     imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
-    facesDetectadas = classificador.detectMultiScale(imagemCinza, scaleFactor=1.5,minSize=(150,150))
+    facesDetectadas = classificador.detectMultiScale(imagemCinza, scaleFactor=1.2,minSize=(150,150))
 
     for (x , y , l, a) in facesDetectadas:
         cv2.rectangle(imagem, (x, y), (x + l, y + a), (0, 0, 255), 3)
