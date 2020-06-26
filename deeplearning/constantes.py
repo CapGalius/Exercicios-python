8a17e3d09cdef19a3316f59b6ba9c9c071d663c3
CapGalius <capgalius@gmail.com>
2020-06-26 10:29:47 -0300

    up

---
+++ deeplearning/constantes.py
@@ -0,0 +1,10 @@
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+valor1 = tf.constant(2)
+valor2 = tf.constant(3)
+import numpy as np
+type(valor1)
+print(valor1)
+x = 3
+y = np.random.rand(3,2)
---
+++ deeplearning/deep.py
@@ -0,0 +1,4 @@
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+import numpy as np
+import matplotlib.pyplot as plt
