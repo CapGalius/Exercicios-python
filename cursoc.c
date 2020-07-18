22a93e09ad80e82ab03940c43d7b727c4e1cda02
CapGalius <capgalius@gmail.com>
2020-07-18 20:22:33 -0300

    up

--- cursoc.c
+++ cursoc.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
+#include <stdlib.h>
 #include<locale.h> // configuração de acentos não remover
 
 int main(){
     setlocale(LC_ALL,NULL); // configuração de acentos não remover do main
-    
+    system("color A"); //Texto em verde -  Saudades do Monitor CGA
+
     int idade;
     printf("Qual é a sua idade? ");
     scanf("%d", &idade);
+    printf("A sua idade é: %d", idade);
 
     return 0;
 
